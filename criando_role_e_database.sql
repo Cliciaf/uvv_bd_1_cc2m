c5b9cb83f9fef45f3c80dd5be5c7241b8aa445b2
Cliciaf <cliciafreitas.21@gmail.com>
2022-04-20 17:58:22 -0300

    script do banco de dados

---
+++ criando_role_e_database.sql
@@ -0,0 +1,21 @@
+--Criando a role
+CREATE ROLE clicia  WITH 
+      SUPERUSER
+      CREATEDB 
+      CREATEROLE
+      INHERIT
+      LOGIN
+      NOREPLICATION 
+      BYPASSRLS
+      CONNECTION LIMIT = -1
+      PASSWORD '1234';
+
+--Criando o banco de dados uvv
+CREATE DATABASE uvv
+     WITH
+      OWNER  clicia 
+      TEMPLATE  template0 
+      ENCODING  'UTF8'
+      LC_COLLATE  'pt_BR.UTF-8' 
+      LC_CTYPE  'pt_BR.UTF-8'
+      ALLOW_CONNECTIONS  true
