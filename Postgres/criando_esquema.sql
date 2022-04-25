88d474875c911eea6c26f442eef7e9165dfdd56e
Cliciaf <cliciafreitas.21@gmail.com>
2022-04-25 12:36:34 -0300

    Organizando pastas e excluindo arquivos desnecessarios

--- Postgres/criando_esquema.sql
+++
@@ -1,7 +0,0 @@
--- Criando o schema elmasri
-CREATE SCHEMA IF NOT EXISTS elmasri
-    AUTHORIZATION clicia;
-
--- trocando para o schema elmasri
-ALTER USER clicia
-SET SEARCH_PATH TO elmasri, 'clicia', public;
