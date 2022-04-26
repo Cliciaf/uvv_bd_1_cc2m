ca413159d5615e4fce46542136f11f692370a0cf
Cliciaf <cliciafreitas.21@gmail.com>
2022-04-26 20:56:27 -0300

    Mariadb concluido

--- pset1/Scripts/MariaDB.sql
+++ pset1/Scripts/MariaDB.sql
@@ -1,12 +1,11 @@
 -- Cria usuário do SGBD
-CREATE USER vinicius IDENTIFIED BY '';
-
--- Concede todos privilégios de administrador ao usuário
-GRANT ALL PRIVILEGES ON uvv.* TO vinicius; 
+CREATE USER clicia IDENTIFIED BY '';
 
 -- Cria Banco de dados UVV com suas propriedades e permissões
 CREATE DATABASE uvv CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
+-- Concede todos privilégios de administrador ao usuário
+GRANT ALL PRIVILEGES ON uvv.* TO clicia;
 --Conectando ao banco de dados uvv
 USE uvv;
 
