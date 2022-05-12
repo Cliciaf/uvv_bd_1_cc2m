8a616ff37abc2c16c8cbe2ee747f58f30ea47b1a
Cliciaf <cliciafreitas.21@gmail.com>
2022-05-12 14:27:35 -0300

    correções

--- pset1/Scripts/Postgres.sql
+++ pset1/Scripts/Postgres.sql
@@ -1,343 +1,173 @@
---Criando a role
-CREATE ROLE clicia  WITH 
-      SUPERUSER
-      CREATEDB 
-      CREATEROLE
-      INHERIT
-      LOGIN
-      NOREPLICATION 
-      BYPASSRLS
-      CONNECTION LIMIT  -1
-      PASSWORD '1234';
+-- Cria o projeto do Elmasri
 
---Criando o banco de dados uvv
-CREATE DATABASE uvv
-     WITH
-      OWNER  clicia
-      TEMPLATE  template0 
-      ENCODING  'UTF8'
-      LC_COLLATE  'pt_BR.UTF-8' 
-      LC_CTYPE  'pt_BR.UTF-8'
-      ALLOW_CONNECTIONS  true;
--- Entrando no banco de dados da uvv
+-- Faz limpeza geral:
+DROP DATABASE IF EXISTS uvv;
+DROP USER IF EXISTS clicia;
 
-\connect uvv
--- Criando o schema uvv.elmasri
-CREATE SCHEMA IF NOT EXISTS elmasri
-    AUTHORIZATION clicia;
+-- Cria o usuário que será o dono do banco de dados:
+CREATE USER clicia WITH
+    NOSUPERUSER
+    CREATEDB
+    CREATEROLE
+    LOGIN
+    ENCRYPTED PASSWORD '1234'
+;
 
--- trocando para o schema uvv.elmasri
-ALTER USER clicia
-SET SEARCH_PATH TO elmasri, 'clicia', public;
+-- Cria o banco de dados UVV:
+CREATE DATABASE uvv WITH
+    owner      = abrantes
+    template   = template0
+    encoding   = 'UTF-8'
+    lc_collate = 'pt_br.UTF-8'
+    lc_ctype   = 'pt_BR.UTF-8'
+;
 
+-- Conexão ao banco uvv com o usuário yuri:
+\c "dbname=uvv user=clicia password=1234"
 
-CREATE TABLE elmasri.funcionario (
-                cpf_funcionario VARCHAR(11) NOT NULL,
-                primeiro_nome VARCHAR(15) NOT NULL,
-                nome_meio CHAR(1),
-                ultimo_nome VARCHAR(15) NOT NULL,
-                data_nascimento DATE,
-                endereco VARCHAR(50),
-                sexo CHAR(1) CHECK(sexo = 'M' or sexo = 'F' or sexo = 'm' or sexo = 'f'),
-                cpf_supervisor VARCHAR(11) CHECK (cpf_supervisor != cpf_funcionario),
-                salario NUMERIC(10,2),
-                numero_departamento INTEGER NOT NULL,
-                CONSTRAINT cpf_funcionario_pk PRIMARY KEY (cpf_funcionario)
+-- Cria o esquema Elamsri:
+CREATE SCHEMA elmasri AUTHORIZATION clicia;
+
+ALTER USER yuri 
+SET SEARCH_PATH TO elmasri, "$user", public;
+
+SET SEARCH_PATH TO elmasri, "$user", public;
+
+-- Cria a tabela funcionário:
+CREATE TABLE funcionario (
+    cpf                 CHAR(11)    CONSTRAINT nn_func_cpf       NOT NULL,
+    primeiro_nome       VARCHAR(15) CONSTRAINT nn_func_prim_nome NOT NULL,
+    nome_meio           CHAR(1),
+    ultimo_nome         VARCHAR(15) CONSTRAINT nn_func_ult_nome  NOT NULL,
+    data_nascimento     DATE,
+    endereco            VARCHAR(35),
+    sexo                CHAR(1),
+    salario             DECIMAL (10,2),
+    cpf_supervisor      CHAR(11),
+    numero_departamento INT
 );
 
--- Comentarios da tabela funcionario
-comment on
-table funcionario is 'Tabela que armazena as informações dos funcionários.';
--- Coluna de comentarios
+-- Cria chaves da tabela funcionário:
+ALTER TABLE funcionario ADD CONSTRAINT pk_funcionario 
+PRIMARY KEY (cpf);
 
-comment on
-column funcionario.cpf is 'CPF do funcionário. Será a PK da tabela.';
+ALTER TABLE funcionario ADD CONSTRAINT fk_cpf_super_cpf
+FOREIGN KEY (cpf_supervisor) REFERENCES funcionario (cpf);
 
-comment on
-column funcionario.primeiro_nome is 'Primeiro nome do funcionário.';
+-- Constraints adicionais para a tabela funcionário:
+ALTER TABLE funcionario ADD CONSTRAINT ck_func_sexo 
+CHECK (sexo IN ('M', 'F'));
 
-comment on
-column funcionario.nome_meio is 'Inicial do nome do meio.';
+ALTER TABLE funcionario ADD CONSTRAINT ck_func_salario
+CHECK (salario >= 0);
 
-comment on
-column funcionario.ultimo_nome is 'Sobrenome do funcionário.';
+-- Comentários da tabela funcionário:
+COMMENT ON TABLE funcionario IS 'Tabela que armazena informações dos funcionários.';
+COMMENT ON COLUMN funcionario.cpf IS '';
+COMMENT ON COLUMN funcionario. IS '';
+COMMENT ON COLUMN funcionario. IS '';
+COMMENT ON COLUMN funcionario. IS '';
+COMMENT ON COLUMN funcionario. IS '';
+COMMENT ON COLUMN funcionario. IS '';
 
-comment on
-column funcionario.endereco is 'Endereço do funcionário.';
-
-comment on
-column funcionario.sexo is 'Sexo do funcionário';
-
-comment on
-column funcionario.salario is 'Salário do funcionário';
-
-comment on
-column funcionario.cpf_supervisor is 'CPF do supervisor. Será uma FK para a própria tabela (um auto-relacionamento).';
-
-comment on
-column funcionario.numero_departamento is 'Número do departamento do funcionário.';
--- End Table Funcionario
-
-
-CREATE TABLE elmasri.dependente (
-                nome_dependente VARCHAR(15) NOT NULL,
-                cpf_funcionario VARCHAR(11) NOT NULL,
-                sexo CHAR(1) CHECK(sexo = 'M' or sexo = 'F' or sexo = 'm' or sexo = 'f'),
-                data_nascimento DATE,
-                parentesco VARCHAR(15),
-                CONSTRAINT cpf_funcionario_dependente_pk PRIMARY KEY (nome_dependente, cpf_funcionario)
+-- Cria a tabela dependente:
+CREATE TABLE dependente (
+    cpf_funcionario CHAR(11)    CONSTRAINT nn_depend_cpf_func NOT NULL,
+    nome_dependente VARCHAR(15) CONSTRAINT nn_depend_nome_dep NOT NULL,
+    sexo            CHAR(1),
+    data_nascimento DATE,
+    parentesco      VARCHAR(15)
 );
 
--- Comentarios da tabela dependentes
-comment on
-table dependente is 'Tabela que armazena as informações dos dependentes dos funcionários.';
--- Comentario por coluna
-comment on
-column dependente.cpf_funcionario is 'CPF do funcionário. Faz parte da PK desta tabela e é uma FK para a tabela funcionário.';
+-- Cria chaves da tabela dependente:
+ALTER TABLE dependente ADD CONSTRAINT pk_dependente 
+PRIMARY KEY (cpf_funcionario, nome_dependente);
 
-comment on
-column dependente.nome_dependente is 'Nome do dependente. Faz parte da PK desta tabela.';
+ALTER TABLE dependente ADD CONSTRAINT fk_cpf_func_cpf
+FOREIGN KEY (cpf_funcionario) REFERENCES funcionario (cpf);
 
-comment on
-column dependente.sexo is 'Sexo do dependente.';
+-- Constraints adicionais para a tabela dependente:
+ALTER TABLE dependente ADD CONSTRAINT ck_depend_sexo
+CHECK (sexo IN ('M', 'F'));
 
-comment on
-column dependente.data_nascimento is 'Data de nascimento do dependente.';
 
-comment on
-column dependente.parentesco is 'Descrição do parentesco do dependente com o funcionário.';
--- Fim dos comentarios da tabela dependentes
-
-CREATE TABLE elmasri.departamento (
-                numero_departamento INTEGER NOT NULL CHECK (numero_departamento > 0 AND numero_departamento < 21),
-                cpf_gerente VARCHAR(11) NOT NULL,
-                nome_departamento VARCHAR(15) NOT NULL,
-                data_inicio_gerente DATE,
-                CONSTRAINT numero_departamento_pk PRIMARY KEY (numero_departamento)
+-- Cria a tabela departamento:
+CREATE TABLE departamento (
+    numero_departamento INT         CONSTRAINT nn_dept_num_dept   NOT NULL,
+    nome_departamento   VARCHAR(15) CONSTRAINT nn_dept_nome_dept  NOT NULL,
+    cpf_gerente         CHAR(11)    CONSTRAINT nn_dept_cpf_gerent NOT NULL,
+    data_inicio         DATE
 );
 
+-- Cria chaves da tabela departamento:
+ALTER TABLE departamento ADD CONSTRAINT pk_departamento
+PRIMARY KEY (numero_departamento);
 
-CREATE UNIQUE INDEX departamento_idx
- ON elmasri.departamento
- ( nome_departamento );
+ALTER TABLE departamento ADD CONSTRAINT fk_cpf_ger_cpf
+FOREIGN KEY (cpf_gerente) REFERENCES funcionario (cpf);
 
--- Table comments departamento
-comment on
-table departamento is 'Tabela que armazena as informaçoẽs dos departamentos.';
--- Coluna de comentarios
-comment on
-column departamento.numero_departamento is 'Número do departamento. É a PK desta tabela.';
+-- ALTER TABLE departamento ADD CONSTRAINT un_dept_nome_dept
+-- UNIQUE (nome_departamento);
 
-comment on
-column departamento.nome_departamento is 'Nome do departamento. Deve ser único.';
+CREATE UNIQUE INDEX uidx_dept_nome_dept ON departamento (nome_departamento);
 
-comment on
-column departamento.cpf_gerente is 'CPF do gerente do departamento. É uma FK para a tabela funcionários.';
+-- Constraints adicionais para a tabela departamento:
+ALTER TABLE departamento ADD CONSTRAINT ck_dept_num_dept
+CHECK (numero_departamento >= 0);
 
-comment on
-column departamento.data_inicio_gerente is 'Data do início do gerente no departamento.';
--- End departamento
-
-
-CREATE TABLE elmasri.localizacoes_departamento (
-                local VARCHAR(50) NOT NULL,
-                numero_departamento INTEGER NOT NULL,
-                CONSTRAINT numero_departamento_localizacao_pk PRIMARY KEY (local, numero_departamento)
+-- Cria a tabela localizações do departamento:
+CREATE TABLE localizacoes_departamento (
+    numero_departamento INT         CONSTRAINT nn_loc_dept_num_dept NOT NULL,
+    local               VARCHAR(15) CONSTRAINT nn_loc_dept_local    NOT NULL
 );
 
--- Comentarios na tabela localizações departamento
-comment on
-table localizacoes_departamento is 'Tabela que armazena as possíveis localizações dos departamentos.';
--- Coluna de comentarios
+-- Cria chaves da tabela localizações do departamento:
+ALTER TABLE localizacoes_departamento ADD CONSTRAINT pk_localizacoes_dept
+PRIMARY KEY (numero_departamento, local);
 
-comment on
-column localizacoes_departamento.numero_departamento is 'Número do departamento. Faz parta da PK desta tabela e também é uma FK para a tabela departamento.';
+ALTER TABLE localizacoes_departamento ADD CONSTRAINT fk_num_dept_num_dept
+FOREIGN KEY (numero_departamento) REFERENCES departamento (numero_departamento);
 
-comment on
-column localizacoes_departamento.local is 'Localização do departamento. Faz parte da PK desta tabela.';
--- End Table localizacoes_departamento
-
-CREATE TABLE elmasri.projeto (
-                numero_projeto INTEGER NOT NULL,
-                numero_departamento INTEGER NOT NULL,
-                nome_projeto VARCHAR(15) NOT NULL,
-                local_projeto VARCHAR(50),
-                CONSTRAINT numero_projeto_pk PRIMARY KEY (numero_projeto)
+-- Cria a tabela projeto:
+CREATE TABLE projeto (
+    numero_projeto      INT         CONSTRAINT nn_proj_num_proj  NOT NULL,
+    nome_projeto        VARCHAR(15) CONSTRAINT nn_proj_nome_proj NOT NULL,
+    local_projeto       VARCHAR(15),
+    numero_departamento INT         CONSTRAINT nn_proj_num_dept  NOT NULL
 );
 
+-- Cria chaves da tabela projeto:
+ALTER TABLE projeto ADD CONSTRAINT pk_projeto
+PRIMARY KEY (numero_projeto);
 
-CREATE UNIQUE INDEX projeto_idx
- ON elmasri.projeto
- ( nome_projeto );
+ALTER TABLE projeto ADD CONSTRAINT fk_proj_num_dept_num_dept
+FOREIGN KEY (numero_departamento) REFERENCES departamento (numero_departamento);
 
- -- Table comment
-comment on
-table projeto is 'Tabela que armazena as informações sobre os projetos dos departamentos.';
--- Coluna de comentarios
+CREATE UNIQUE INDEX uidx_proj_nome_proj ON projeto (nome_projeto);
 
-comment on
-column projeto.numero_projeto is 'Número do projeto. É a PK desta tabela.';
-
-comment on
-column projeto.nome_projeto is 'Nome do projeto. Deve ser único.';
-
-comment on
-column projeto.local_projeto is 'Localização do projeto.';
-
-comment on
-column projeto.numero_departamento is 'Número do departamento. É uma FK para a tabela departamento.';
--- End Table projeto
-
-CREATE TABLE elmasri.trabalha_em (
-                cpf_funcionario VARCHAR(11) NOT NULL,
-                numero_projeto INTEGER NOT NULL,
-                horas NUMERIC(3,1),
-                CONSTRAINT cpf_funcionario_trabalhaem_pk PRIMARY KEY (cpf_funcionario, numero_projeto)
+-- Cria a tabela trabalha em:
+CREATE TABLE trabalha_em (
+    cpf_funcionario CHAR(11) CONSTRAINT nn_trab_em_cpf      NOT NULL,
+    numero_projeto  INT      CONSTRAINT nn_trab_em_num_proj NOT NULL,
+    horas           DECIMAL(3,1)
 );
 
--- Table comment
-comment on
-table trabalha_em is 'Tabela para armazenar quais funcionários trabalham em quais projetos.';
--- Coluna de comentarios
+-- Cria chaves da tabela trabalha_em:
+ALTER TABLE trabalha_em ADD CONSTRAINT pk_trabalha_em
+PRIMARY KEY (cpf_funcionario, numero_projeto);
 
-comment on
-column trabalha_em.cpf_funcionario is 'CPF do funcionário. Faz parte da PK desta tabela e é uma FK para a tabela funcionário.';
+ALTER TABLE trabalha_em ADD CONSTRAINT fk_trab_em_cpf_func_cpf
+FOREIGN KEY (cpf_funcionario) REFERENCES funcionario (cpf);
 
-comment on
-column trabalha_em.numero_projeto is 'Número do projeto. Faz parte da PK desta tabela e é uma FK para a tabela projeto.';
+ALTER TABLE trabalha_em ADD CONSTRAINT fk_trab_em_num_proj_num_proj
+FOREIGN KEY (numero_projeto) REFERENCES projeto (numero_departamento)
 
-comment on
-column trabalha_em.horas is 'Horas trabalhadas pelo funcionário neste projeto.';
--- End Table trabalha_em
-
+-- Constraints adicionais para a tabela trabalha em:
+ALTER TABLE trabalha_em ADD CONSTRAINT ck_trab_em_horas
+CHECK (horas >= 0);
 
 
-ALTER TABLE elmasri.trabalha_em ADD CONSTRAINT funcionario_trabalha_em_fk
-FOREIGN KEY (cpf_funcionario)
-REFERENCES elmasri.funcionario (cpf_funcionario)
-ON DELETE SET DEFAULT
-ON UPDATE CASCADE
-;
-
-ALTER TABLE elmasri.funcionario ADD CONSTRAINT funcionario_funcionario_fk
-FOREIGN KEY (cpf_supervisor)
-REFERENCES elmasri.funcionario (cpf_funcionario)
-ON DELETE SET DEFAULT
-ON UPDATE CASCADE
-;
-
-ALTER TABLE elmasri.departamento ADD CONSTRAINT funcionario_departamento_fk
-FOREIGN KEY (cpf_gerente)
-REFERENCES elmasri.funcionario (cpf_funcionario)
-ON DELETE SET DEFAULT
-ON UPDATE CASCADE
-;
-
-ALTER TABLE elmasri.departamento ADD CONSTRAINT funcionario_departamento_fk1
-FOREIGN KEY (cpf_gerente)
-REFERENCES elmasri.funcionario (cpf_funcionario)
-ON DELETE SET DEFAULT
-ON UPDATE CASCADE
-;
-
-ALTER TABLE elmasri.dependente ADD CONSTRAINT funcionario_dependente_fk
-FOREIGN KEY (cpf_funcionario)
-REFERENCES elmasri.funcionario (cpf_funcionario)
-ON DELETE SET DEFAULT
-ON UPDATE CASCADE
-;
-
-ALTER TABLE elmasri.projeto ADD CONSTRAINT departamento_projeto_fk
-FOREIGN KEY (numero_departamento)
-REFERENCES elmasri.departamento (numero_departamento)
-ON DELETE SET DEFAULT
-ON UPDATE CASCADE
-;
-
-ALTER TABLE elmasri.localizacoes_departamento ADD CONSTRAINT departamento_localizacoes_departamento_fk
-FOREIGN KEY (numero_departamento)
-REFERENCES elmasri.departamento (numero_departamento)
-ON DELETE SET DEFAULT
-ON UPDATE CASCADE
-;
-
-ALTER TABLE elmasri.trabalha_em ADD CONSTRAINT projeto_trabalha_em_fk
-FOREIGN KEY (numero_projeto)
-REFERENCES elmasri.projeto (numero_projeto)
-ON DELETE SET DEFAULT
-ON UPDATE CASCADE
-;
-
--- Inserindo objetos
---Inserindo objetos na tabela funcionario
-INSERT INTO elmasri.funcionario
-    (primeiro_nome, nome_meio, ultimo_nome, cpf_funcionario, data_nascimento, endereco, sexo, salario, cpf_supervisor, numero_departamento) 
-        VALUES
-        ('Jorge'    , 'E'   , 'Brito'    , '88866555576'    , '10-11-1937'  , 'R.do Horto,35,São Paulo,SP'            , 'M' , 55000 , NULL          , 1),
-        ('Fernando' , 'T'   , 'Wong'     , '33344555587'    , '1955-12-08'  , 'R.da Lapa,34,São Paulo,SP'             , 'M' , 40000 , '88866555576' , 5),
-        ('João'     , 'B'   , 'Silva'    , '12345678966'    , '1965-01-09'  , 'R.das Flores,751,São Paulo,SP'         , 'M' , 30000 , '33344555587' , 5),
-        ('Jennifer' , 'S'   , 'Souza'    , '98765432168'    , '1941-06-20'  , 'Av. Arthur de Lima,54,Santo André,SP'  , 'F' , 43000 , '88866555576' , 4),
-        ('Ronaldo'  , 'K'   , 'Lima'     , '66688444476'    , '1962-09-15'  , 'R.Rebouças,65,Piracicaba,SP'           , 'M' , 38000 , '33344555587' , 5),
-        ('Joice'    , 'A'   , 'Leite'    , '45345345376'    , '1972-07-31'  , 'Av.Lucas Obes,74,São Paulo,SP'         , 'F' , 25000 , '33344555587' , 5),
-        ('André'    , 'V'   , 'Perreira' , '98798798733'    , '1969-03-29'  , 'R.Timbira,35,São Paulo,SP'             , 'M' , 25000 , '98765432168' , 4),
-        ('Alice'    , 'J'   , 'Zelaya'   , '99988777767'    , '1968-01-19'  , 'R.Souza Lima,35,Curitiba,PR'           , 'F' , 25000 , '98765432168' , 4)
-;
-
---Inserindo objetos na tabela departamento
-INSERT INTO elmasri.departamento
-(numero_departamento, cpf_gerente, nome_departamento, data_inicio_gerente)
-	VALUES  
-    (5, '33344555587', 'Pesquisa', '1988-05-22'),
-	(4,'98765432168', 'Administração', '1995-01-01'),
-	(1,'88866555576', 'Matriz', '1981-06-19')
-	;
---Inserindo objetos na tabela localizacoes departamento
-    INSERT INTO elmasri.localizacoes_departamento
-    (local, numero_departamento)
-	VALUES 
-    ('São Paulo', 1),
-	('Mauá', 4),
-	('Santo André', 5),
-	('Itu', 5),
-	('São Paulo', 5);
--- Inserindo objetos na tabela projeto
-INSERT INTO elmasri.projeto
-(numero_projeto, numero_departamento, nome_projeto, local_projeto)
-	VALUES 	
-    (1, 5, 'ProdutoX', 'Santo André'),
-	(2, 5, 'ProdutoY', 'Itu'),
-	(3, 5, 'ProdutoZ', 'São Paulo'),
-	(10, 4, 'Informatização', 'Mauá'),
-	(20, 1,'Reorganização' , 'São Paulo'),
-	(30, 4, 'Novosbeneficios', 'Mauá');
-
-INSERT INTO elmasri.dependente(
-	nome_dependente, cpf_funcionario, sexo, data_nascimento, parentesco)
-	VALUES
-    ('Alicia','33344555587','F','1986-04-05','Filha'),
-    ('Tiago','33344555587','M','1983-10-25','Filho'),
-    ('Janaia','33344555587','F','1958-05-03','Esposa'),
-    ('Antonio','98765432168','M','1942-02-28','Marido'),
-    ('Michel','12345678966','M', '1988-01-04','Filho'),
-    ('Alicia','12345678966','F', '1988-12-30','Filha'),
-    ('Elizabeth','12345678966','F', '1967-05-05','Esposa');
-
-INSERT INTO elmasri.trabalha_em(
-	cpf_funcionario, numero_projeto, horas)
-	VALUES 
-    ('12345678966',1,32.5),
-    ('12345678966',2,7.5),
-    ('66688444476',3,40.0),
-    ('45345345376',1,20.0),
-    ('45345345376',2,20.0),
-    ('33344555587',2,10.0),
-    ('33344555587',3,10.0),
-    ('33344555587',10,10.0),
-    ('33344555587',20,10.0),
-    ('99988777767',30,30.0),
-    ('99988777767',10,10.0),
-    ('98798798733',10,35.0),
-    ('98798798733',30,5.0),
-    ('98765432168',30,20.0),
-    ('98765432168',20,15.0),
-    ('88866555576',20,NULL);
+-- Inserção de dados
+-- Tabela funcionário
+INSERT INTO funcionario (primeiro_nome, nome_meio, ultimo_nome, cpf, data_nascimento, endereco, sexo, salario, cpf_supervisor, numero_departamento) VALUES (
+    'Jorge', 'E', 'Brito', '88866555576', '1937-11-10', 'Rua do Horto, 35, São Paulo, SP', 'M', 55000, null, null);
