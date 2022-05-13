eed370d5fca362e75cc77aae46c7b4d18c79fe3c
Cliciaf <cliciafreitas.21@gmail.com>
2022-05-12 21:02:47 -0300

    ajustes pset1 e criação pset2

--- pset1/Scripts/Postgres.sql
+++ pset1/Scripts/Postgres.sql
@@ -15,7 +15,7 @@ CREATE USER clicia WITH
 
 -- Cria o banco de dados UVV:
 CREATE DATABASE uvv WITH
-    owner      = abrantes
+    owner      = clicia
     template   = template0
     encoding   = 'UTF-8'
     lc_collate = 'pt_br.UTF-8'
@@ -28,7 +28,7 @@ CREATE DATABASE uvv WITH
 -- Cria o esquema Elamsri:
 CREATE SCHEMA elmasri AUTHORIZATION clicia;
 
-ALTER USER yuri 
+ALTER USER clicia 
 SET SEARCH_PATH TO elmasri, "$user", public;
 
 SET SEARCH_PATH TO elmasri, "$user", public;
@@ -167,7 +167,66 @@ ALTER TABLE trabalha_em ADD CONSTRAINT ck_trab_em_horas
 CHECK (horas >= 0);
 
 
--- Inserção de dados
--- Tabela funcionário
-INSERT INTO funcionario (primeiro_nome, nome_meio, ultimo_nome, cpf, data_nascimento, endereco, sexo, salario, cpf_supervisor, numero_departamento) VALUES (
-    'Jorge', 'E', 'Brito', '88866555576', '1937-11-10', 'Rua do Horto, 35, São Paulo, SP', 'M', 55000, null, null);
+-- Inserção de dados
+INSERT INTO elmasri.funcionario (primeiro_nome, nome_meio, ultimo_nome, cpf, data_nascimento, endereco, sexo, salario, cpf_supervisor, numero_departamento) 
+	VALUES 
+	('Jorge', 'E', 'Brito', '88866555576', '1937-11-10', 'Rua do Horto,35,São Paulo,SP', 'M', 55000, null, 1),
+	('Fernando', 'T', 'Wong', '33344555587', '1955-12-08', 'Rua da Lapa,34,São Paulo,SP', 'M', 40000, '88866555576', 5),
+	('João', 'B', 'Silva', '12345678966', '1965-01-09', 'Rua das Flores, 751, São Paulo,SP', 'M', 30000, '33344555587', 5),
+	('Jennifer', 'S', 'Souza', '98765432168', '1941-06-20', 'Av. Arthur de Lima,54,SantoAndré,SP', 'F', 43000, '88866555576', 4),
+	('Ronaldo', 'K', 'Lima', '66688444476', '1962-09-15', 'Rua Rebouças,65,Piracicaba,SP', 'M', 38000, '33344555587', 5),
+	('Joice', 'A', 'Leite', '45345345376', '1972-07-31', 'Av. Lucas Obes,74,São Paulo,SP', 'F', 25000, '33344555587', 5),
+	('André', 'V', 'Pereira', '98798798733', '1969-03-29', 'Rua Timbira,35,São Paulo,SP', 'M', 25000, '98765432168', 4),
+	('Alice', 'J', 'Zelaya', '99988777767', '1968-01-19', 'Rua Souza Lima,35,Curitiba,PR', 'F', 25000, '98765432168', 4);
+
+INSERT INTO elmasri.departamento (nome_departamento, numero_departamento, cpf_gerente, data_inicio) 
+	VALUES
+	('Pesquisa', 5, '33344555587', '1988-05-22'),
+	('Administração', 4, '98765432168', '1995-01-01'),
+	('Matriz', 1, '88866555576', '1981-06-19');
+	
+INSERT INTO  elmasri.localizacoes_departamento (numero_departamento, local) 
+	VALUES
+	(1, 'São Paulo'),
+	(4, 'Mauá'),
+	(5, 'Santo André'),
+	(5, 'Itu'),
+	(5, 'São Paulo');
+	
+INSERT INTO elmasri.projeto (nome_projeto, numero_projeto, local_projeto, numero_departamento)
+	VALUES
+	('ProdutoX', 1, 'Santo André', 5),
+	('ProdutoY', 2, 'Itu', 5),
+	('ProdutoZ', 3, 'São Paulo', 5),
+	('Informatização', 10, 'Maué', 4),
+	('Reorganização', 20, 'São Paulo', 1),
+	('Novosbenefícios', 30, 'Mauá', 4);
+	
+INSERT INTO elmasri.dependente (cpf_funcionario, nome_dependente, sexo, data_nascimento, parentesco)
+	VALUES
+	('33344555587', 'Alicia', 'F', '1986-04-05', 'Filha'),
+	('33344555587', 'Tiago', 'M', '1983-10-25', 'Filho'),
+	('33344555587', 'Janaína', 'F', '1958-05-03', 'Esposa'),
+	('98765432168', 'Antonio', 'M', '1942-02-28', 'Marido'),
+	('12345678966', 'Michael', 'M', '1988-01-04', 'Filho'),
+	('12345678966', 'Alicia', 'F', '1988-12-30', 'Filha'),
+	('12345678966', 'Elizabeth', 'F', '1967-05-05', 'Esposa');
+	
+INSERT INTO elmasri.trabalha_em (cpf_funcionario, numero_projeto, horas)
+	VALUES
+	('12345678966', 1, 32.5),
+	('12345678966', 2, 7.5),
+	('66688444476', 3, 40.0),
+	('45345345376', 1, 20.0),
+	('45345345376', 2, 20.0),
+	('33344555587', 2, 10.0),
+	('33344555587', 3, 10.0),
+	('33344555587', 10, 10.0),
+	('33344555587', 20, 10.0),
+	('99988777767', 30, 30.0),
+	('99988777767', 10, 10.0),
+	('98798798733', 10, 35.0),
+	('98798798733', 30, 5.0),
+	('98765432168', 30, 20.0),
+	('98765432168', 20, 15.0),
+	('88866555576', 20, null);
---
+++ pset2/questões.sql
@@ -0,0 +1,128 @@
+-- 1 Prepare um relatório que mostre a média salarial dos funcionários de cada departamento.
+
+SELECT d.nome_departamento , CAST(AVG(salario) AS DECIMAL (10,2)) AS media_salario  
+FROM funcionario AS f
+INNER JOIN
+departamento AS d ON d.numero_departamento = f.numero_departamento
+GROUP BY d.nome_departamento;
+
+-- 2 prepare um relatório que mostre a média salarial dos homens e das mulheres.
+
+
+SELECT (CASE WHEN(f.sexo = 'M') THEN 'Homens' END) AS sexo, CAST(AVG(salario) AS DECIMAL(10, 2)) AS media_por_sexo
+FROM funcionario AS f
+WHERE f.sexo = 'M'
+GROUP BY f.sexo
+UNION
+SELECT (CASE WHEN(f.sexo = 'F') THEN 'Mulheres' END) AS sexo, CAST(AVG(salario) AS DECIMAL(10, 2)) AS media_por_sexo
+FROM funcionario AS f
+WHERE f.sexo = 'F'
+GROUP BY f.sexo;
+
+-- 3 prepare um relatório que liste o nome dos departamentos e, para cada departamento, inclua as seguintes informações de seus funcionários: o nome completo, a data de nascimento, a idade em anos completos e o salário.
+
+SELECT d.nome_departamento, (f.primeiro_nome||' '||f.nome_meio||' '||f.ultimo_nome) AS nome_completo, DATE_PART('year', AGE(f.data_nascimento)) AS idade, f.salario
+FROM departamento AS d
+INNER JOIN funcionario AS f
+ON d.numero_departamento = f.numero_departamento;
+
+-- 4 Relatório do nome do departamento e o nome completo, data de nascimento, idade e salário de cada funcionário
+
+SELECT d.nome_departamento, (f.primeiro_nome ||' '|| f.nome_meio ||' '|| f.ultimo_nome) AS nome_completo, f.data_nascimento, DATE_PART('year', AGE(f.data_nascimento)) AS idade, CAST(f.salario AS DECIMAL(10,2))
+FROM funcionario AS f 
+INNER JOIN
+departamento AS d ON (f.numero_departamento = d.numero_departamento);
+
+-- 5 Relatório que apresenta o nome completo dos funcionários, idade, salário atual e o salário reajustado em 20% caso o atual seja inferior à 35 mil ou em 15% caso seja superior a esse valor
+
+SELECT (f.primeiro_nome ||' '|| f.nome_meio ||' '|| f.ultimo_nome) AS nome_completo, DATE_PART('year', AGE(f.data_nascimento)) AS idade, CAST(f.salario AS DECIMAL(10,2)) AS salario_atual, CAST(f.salario*1.2 AS DECIMAL(10,2)) AS salario_reajustado
+FROM funcionario AS f, departamento AS d
+WHERE salario < 35000 AND f.numero_departamento = d.numero_departamento
+UNION
+SELECT DISTINCT (f.primeiro_nome ||' '|| f.nome_meio ||' '|| f.ultimo_nome) AS nome_completo, DATE_PART('year', AGE(f.data_nascimento)) AS idade, CAST(f.salario AS DECIMAL(10,2)) AS salario_atual, CAST(f.salario*1.15 AS DECIMAL(10,2)) AS salario_reajustado
+FROM funcionario AS f, departamento AS d
+WHERE salario >= 35000 AND f.numero_departamento = d.numero_departamento;
+
+-- 6  Relatório com a lista o nome do gerente e dos funcionários de cada departamento e ordena por nome do departamento (em ordem crescente) e pelo salário dos funcionários (em ordem decrescente)
+SELECT d.nome_departamento, (CASE WHEN(d.cpf_gerente = '88866555576') THEN 'Jorge E Brito' WHEN(d.cpf_gerente = '98765432168') THEN 'Jennifer S Souza' WHEN(d.cpf_gerente = '33344555587') THEN 'Fernando T Wong' END) AS nome_gerente, (f.primeiro_nome ||' '|| f.nome_meio ||' '|| f.ultimo_nome) AS nome_funcionario, f.salario
+FROM departamento AS d, funcionario AS f
+WHERE d.numero_departamento = f.numero_departamento
+ORDER BY d.nome_departamento ASC, f.salario DESC;
+
+-- 7 Relatório com o nome completo dos funcionários que possuem pelo menos um dependente, o departamento onde trabalham e o nome completo, idade e sexo do(s) dependente(s)
+SELECT dt.nome_departamento, (f.primeiro_nome ||' '|| f.nome_meio ||' '|| f.ultimo_nome) AS nome_funcionário, dp.nome_dependente, DATE_PART('year', AGE(dp.data_nascimento)) AS idade_dependente, (CASE WHEN(dp.sexo = 'M') THEN 'Masculino' WHEN (dp.sexo = 'F') THEN 'Feminino' END) AS sexo
+FROM funcionario AS f
+INNER JOIN
+dependente AS dp ON dp.cpf_funcionario = f.cpf
+INNER JOIN
+departamento AS dt ON dt.numero_departamento = f.numero_departamento;
+
+-- 8 Relatório com o nome completo, departamento e salário de cada funcionário que não possui dependente
+SELECT (f.primeiro_nome ||' '|| f.nome_meio ||' '|| f.ultimo_nome) AS nome_funcionário, d.nome_departamento, CAST(salario AS DECIMAL(10,2))
+FROM funcionario AS f, departamento AS d
+WHERE f.cpf NOT IN (SELECT d.cpf_funcionario FROM dependente AS d) AND d.numero_departamento = f.numero_departamento;
+
+-- 9 Relatório que mostra, para cada departamento, os seus projetos e nome completo de cada funcionário alocado em cada projeto e o número de horas trabalhadas
+SELECT d.nome_departamento, p.nome_projeto, (f.primeiro_nome ||' '|| f.nome_meio ||' '|| f.ultimo_nome) AS nome_funcionário, CAST(t.horas AS DECIMAL(3,1)) 
+FROM funcionario AS f
+INNER JOIN
+trabalha_em AS t ON f.cpf = t.cpf_funcionario
+INNER JOIN 
+projeto AS p ON t.numero_projeto = p.numero_projeto
+INNER JOIN
+departamento AS d ON d.numero_departamento = p.numero_departamento
+ORDER BY d.nome_departamento ASC, p.nome_projeto ASC;
+
+-- 10 Relatório da soma total das horas de cada projeto em cada departamento, o nome do departamento e do projeto
+SELECT d.nome_departamento, p.nome_projeto, SUM(t.horas) AS total_horas
+FROM trabalha_em AS t
+INNER JOIN 
+projeto AS p ON p.numero_projeto = t.numero_projeto
+INNER JOIN
+departamento AS d ON d.numero_departamento = p.numero_departamento
+GROUP BY d.nome_departamento, p.nome_projeto
+ORDER BY d.nome_departamento ASC;
+
+-- 11 Relatório do nome completo do funcionário, nome do projeto em que ele trabalha e o valor que ele receberá referente às horas trabalhadas neles, considerando 50 reais por hora
+SELECT (f.primeiro_nome ||' '|| f.nome_meio ||' '|| f.ultimo_nome) AS nome_funcionário, p.nome_projeto, CAST(t.horas*50 AS DECIMAL (10,2)) AS valor_pagamento
+FROM funcionario AS f
+INNER JOIN
+trabalha_em AS t ON t.cpf_funcionario = f.cpf
+INNER JOIN
+projeto AS p ON p.numero_projeto = t.numero_projeto;
+
+-- 12 Relatório que exibe o nome do departamento, do projeto e o nome dos funcionários que não registraram nenhuma hora trabalhada
+SELECT d.nome_departamento, p.nome_projeto, (f.primeiro_nome ||' '|| f.nome_meio ||' '|| f.ultimo_nome) AS nome_funcionário, (CASE WHEN(t.horas IS NULL) THEN 'Nenhuma' END) AS horas_trabalhadas
+FROM funcionario AS f
+INNER JOIN
+trabalha_em AS t ON t.cpf_funcionario = f.cpf AND t.horas IS NULL
+INNER JOIN
+projeto AS p ON p.numero_projeto = t.numero_projeto
+INNER JOIN
+departamento AS d ON d.numero_departamento = p.numero_departamento;
+
+-- 13 Relatório que exibe o nome completo, sexo e idade de todos os funcionários e seus dependentes em ordem decrescente da idade em anos completos
+SELECT (f.primeiro_nome ||' '|| f.nome_meio ||' '|| f.ultimo_nome) AS nome, f.sexo, DATE_PART('year', AGE(f.data_nascimento)) AS idade
+FROM funcionario AS f
+UNION
+SELECT dp.nome_dependente AS nome, dp.sexo, DATE_PART('year', AGE(dp.data_nascimento)) AS idade
+FROM dependente AS dp
+ORDER BY idade DESC;
+
+-- 14 Relatório que exibe a quantidade de funcionários de cada departamento
+SELECT d.nome_departamento, COUNT(f.cpf) AS numero_funcionarios
+FROM funcionario AS f
+INNER JOIN
+departamento AS d ON d.numero_departamento = f.numero_departamento
+GROUP BY d.nome_departamento;
+
+-- 15 Relatório com o nome completo dos funcionários, seu departamento e o nome dos projetos em que cada um está alocado
+SELECT (f.primeiro_nome ||' '|| f.nome_meio ||' '|| f.ultimo_nome) AS nome_funcionario, d.nome_departamento, p.nome_projeto
+FROM funcionario AS f
+INNER JOIN
+departamento AS d ON f.numero_departamento = d.numero_departamento
+INNER JOIN 
+trabalha_em AS t ON t.cpf_funcionario = f.cpf
+INNER JOIN
+projeto AS p ON p.numero_projeto = t.numero_projeto
+ORDER BY nome_funcionario ASC;
